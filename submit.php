fb2bc4f6a4088f3a65893b96bdc4b1fd98c11321
Dhananjayan <dhananjayan.online@outlook.com>
2021-11-15 17:46:42 +0530

    vote error updated, submit page connected, and updated

--- submit.php
+++ submit.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Thank you</title>
+  <title>Internet Voting System</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -32,6 +32,8 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+
+  
 </head>
 
 <body>
@@ -62,18 +64,19 @@
   <main id="main">
     
     <!-- ======= Get Started Section ======= -->
-    <section id="get-started" class="padd-section text-center">
+    <section id="hero" class="padd-section text-center">
         <br><br>
         <h1 style="text-align: center;" ><span>Thank You</span></h1>      
       <div class="container">
         <div class="row">
-
-            
-          <h2>Your Vote is recorded..</h2>
-          
-          
+          <h2 style="text-align: center;">Your vote has been recorded..</h2>
         </div>
-        <br><br><br><br><br><br><br><br><br>
+        
+        <br><br><br><br>
+        
+        <br><br><br><br>
+        <a href="index.php" class="btn-get-started scrollto">Go back to home page </a>
+        <br>
       </div>
 
     </section><!-- End Get Started Section -->
@@ -83,23 +86,91 @@
 
   <!-- ======= Footer ======= -->
   <footer class="footer">
-    
-    <!--
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12 col-lg-4">
+          <div class="footer-logo">
+
+            <a class="navbar-brand" href="#">i-Voting System</a>
+            <p>An Internet voting system (i-Voting) is a web based system that allows 
+              groups to securely conduct votes and elections. With i-Voting technology, votes are 
+              collected, counted and managed in an automated fashion</p>
+            <a class="navbar-brand" href="https://cmscollege.ac.in/"><h4 style="color:white";>CMS College Kottayam <br><h5 style="color:white";>(AUTONOMOUS)</h5></h4></a>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>ACADEMICS</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="https://cmscollege.ac.in/syllabus.html">Curriculum</a></li>
+              <li><a href="http://cmscollege.ac.in/examination.html">Examinations</a></li>
+              <li><a href="http://cmscollege.ac.in/all-departments-list.html">All Departments</a></li>
+              <li><a href="https://services.cmscollege.ac.in/Feedback/Feedback/Home">Faculty Evaluation</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>ADMISSIONS</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="https://cmscollege.ac.in/notification-2.html">Notifications</a></li>
+              <li><a href="https://cmscollege.ac.in/programs-offered.html">Programs Offered</a></li>
+              <li><a href="http://cmscollege.ac.in/online-admission.html">UG Online Admission</a></li>
+              <li><a href="http://cmscollege.ac.in/pg-admission.html">PG Online Admission</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>FUTURE STUDENTS</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="https://cmscollege.ac.in/doctoral-programs.html">Doctoral Programs</a></li>
+              <li><a href="https://cmscollege.ac.in/post-graduate-programs.html">Postgraduate Programs</a></li>
+              <li><a href="http://cmscollege.ac.in/ug-programmes-offered.html">Undergraduate Programmes</a></li>              
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>CAMPUS LIFE</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="https://cmscollege.ac.in/complaints-and-grievances.html">Complaints and Grievances</a></li>
+              <li><a href="https://cmscollege.ac.in/students-support-cms.html">Students Support @CMS</a></li>
+              <li><a href="http://cmscollege.ac.in/right-to-information-rti.html">Right to Information (RTI)</a></li>
+              <li><a href="https://cmscollege.ac.in/campus-placements.html">Campus Placements</a></li>
+              <li><a href="http://cmscollege.ac.in/anti-ragging-cell.html">Anti Ragging Cell</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
     <div class="copyrights">
       <div class="container">
-        <p>&copy; Copyrights eStartup. All rights reserved.</p>
+        <p>&copy; Copyrights Internet Voting System. All rights reserved.</p>
         <div class="credits">
-          
-          All the links in the footer should remain intact.
-          You can delete the links only if you purchased the pro version.
-          Licensing information: https://bootstrapmade.com/license/
-          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eStartup
-        
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          Designed by Dhananajayan H
         </div>
       </div>
     </div>
-    -->
 
   </footer><!-- End  Footer -->
 
--- vote-status.php
+++ vote-status.php
@@ -7,13 +7,20 @@ session_start();
 $con = new mysqli('localhost','root','123456','ivs_data_base');
 
 if(isset($_POST["vote"])){
-    $sql="update votter_registration set VOTE='1' where EMAILID='{$_POST['chk']}'";
-    $sql1="update candidate_registration set STATUS=STATUS+1 where C_UPRN='{$_POST['cand']}'";
+    
+    $sql="update votter_registration set VOTE=1 where EMAILID='{$_POST['chk']}'";
+    error_reporting(0);
     $res=$con->query($sql);
-    $resq=$con->query($sql1);
-    if($res->num_rows>0 and $resq->num_rows>0)
+    header('location:submit.php');
+    if($res->num_rows>0 )
     {
+        
+        $sql1="update candidate_registration set STATUS=STATUS+1 where C_UPRN='{$_POST['cand']}'";
+        $resq=$con->query($sql1);
+        if($resq->num_rows>0)
+        {
         header('location:index.php');
+        }
     }
 
 
